c59af32755784594b0d2dc5f58253745321ee372
Romanich <lorancem@mail.ru>
2025-12-21 15:21:24 +0300

    add extension MultiUser

--- src/test/java/api/users/CreateUserTest.java
+++ src/test/java/api/users/CreateUserTest.java
@@ -3,10 +3,7 @@ package api.users;
 import api.BaseTest;
 import api.generators.RandomData;
 import api.models.error.ErrorResponse;
-import api.models.users.AuthUser;
-import api.models.users.User;
-import api.models.users.CreateUserRequest;
-import api.models.users.CreateUserResponse;
+import api.models.users.*;
 import api.requests.skelethon.Endpoint;
 import api.requests.skelethon.requesters.CrudRequester;
 import api.requests.skelethon.requesters.ValidatedCrudRequester;
@@ -16,6 +13,8 @@ import api.specs.ResponseSpecs;
 import common.annotations.WithAuthUser;
 import common.messages.UserErrorMessage;
 import common.extensions.AuthUserExtension;
+import common.storage.UserSession;
+import common.storage.UserSessionStore;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -41,22 +40,14 @@ public class CreateUserTest extends BaseTest {
     @DisplayName("Успешное создание пользователя")
     @MethodSource("userCredentials")
     public void successfulCreationUserWithValidData(String username, String password) {
-        CreateUserRequest request = CreateUserRequest.builder()
-                .username(username)
-                .password(password)
-                .build();
-        CreateUserResponse response = new ValidatedCrudRequester<CreateUserResponse>(RequestSpecs.adminAuthSpec(),
-                Endpoint.USER_CREATE, ResponseSpecs.requestReturnsOK())
-                .post(request);
+        UserSession user = UserSessionStore.create(username, password, Roles.USER_ROLE);
         List<User> users = AdminSteps.getAllUsers();
 
-
-        softly.assertThat(username.toLowerCase()).as("Проверка username в ответе").isEqualTo(response.getUsername());
+        softly.assertThat(username).as("Проверка username в ответе").isEqualTo(user.getAuthUser().getUsername());
         softly.assertThat(users).as("Проверка юзера в общем списке")
-                .extracting(User::getUsername, User::getId)
-                .containsOnlyOnce(tuple(response.getUsername().toLowerCase(), response.getId()));
-
-        AdminSteps.deleteUser(response);
+                .extracting(u -> u.getUsername().toLowerCase(),
+                        User::getId)
+                .containsOnlyOnce(tuple(user.getAuthUser().getUsername().toLowerCase(), user.getAuthUser().getId()));
     }
 
     @DisplayName("Неуспешное создание пользователя, одинаковое username")
