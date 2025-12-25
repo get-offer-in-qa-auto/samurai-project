ad9bf5d8ee161c991b9c9fcdd7e88568ebcf120c
Romanich <lorancem@mail.ru>
2025-12-25 22:34:12 +0300

    add allure report for users tests

--- src/main/java/ui/pages/users/CreateUserPage.java
+++ src/main/java/ui/pages/users/CreateUserPage.java
@@ -1,10 +1,11 @@
 package ui.pages.users;
 
 import com.codeborne.selenide.SelenideElement;
+import common.helpers.StepLogger;
 import ui.pages.BasePage;
 
-import static com.codeborne.selenide.Condition.enabled;
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.enabled;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class CreateUserPage extends BasePage<CreateUserPage> {
@@ -18,48 +19,62 @@ public class CreateUserPage extends BasePage<CreateUserPage> {
     }
 
     public CreateUserPage createUser(String name, String password) {
-        usernameField.shouldBe(visible, enabled).setValue(name);
-        newPasswordField.shouldBe(visible, enabled).setValue(password);
-        confirmPasswordField.shouldBe(visible, enabled).setValue(password);
-        createUserButton.click();
-        return this;
+        return StepLogger.log("Создать пользователя", () -> {
+            usernameField.shouldBe(visible, enabled).setValue(name);
+            newPasswordField.shouldBe(visible, enabled).setValue(password);
+            confirmPasswordField.shouldBe(visible, enabled).setValue(password);
+            createUserButton.click();
+            return this;
+        });
     }
 
     public CreateUserPage createUserWithConfirm(String name, String password, String confirmPassword) {
-        usernameField.shouldBe(visible, enabled).setValue(name);
-        newPasswordField.shouldBe(visible, enabled).setValue(password);
-        confirmPasswordField.shouldBe(visible, enabled).setValue(confirmPassword);
-        createUserButton.click();
-        return this;
+        return StepLogger.log("Создать пользователя с подтверждением пароля", () -> {
+            usernameField.shouldBe(visible, enabled).setValue(name);
+            newPasswordField.shouldBe(visible, enabled).setValue(password);
+            confirmPasswordField.shouldBe(visible, enabled).setValue(confirmPassword);
+            createUserButton.click();
+            return this;
+        });
     }
 
     public CreateUserPage fillFieldsWithoutUsername(String password) {
-        newPasswordField.shouldBe(visible, enabled).setValue(password);
-        confirmPasswordField.shouldBe(visible, enabled).setValue(password);
-        createUserButton.click();
-        return this;
+        return StepLogger.log("Заполнить все поля кроме username", () -> {
+            newPasswordField.shouldBe(visible, enabled).setValue(password);
+            confirmPasswordField.shouldBe(visible, enabled).setValue(password);
+            createUserButton.click();
+            return this;
+        });
     }
 
     public CreateUserPage clickOnCreateButton() {
-        createUserButton.click();
-        return this;
+        return StepLogger.log("Нажать на кнопку Create", () -> {
+            createUserButton.click();
+            return this;
+        });
     }
 
     public UsersPage createUserAndGoToUsers(String name, String password) {
-        createUser(name, password);
-        return getPage(UsersPage.class);
+        return StepLogger.log("Создать пользователя и перейти на страницу всех пользователей", () -> {
+            createUser(name, password);
+            return getPage(UsersPage.class);
+        });
     }
 
     public UsersPage cancelCreateAndGoToUsers(String name, String password) {
-        fillRequiredFieldsAndClickCancel(name, password);
-        return getPage(UsersPage.class);
+        return StepLogger.log("Отменить создание пользователя и перейти на страницу всех пользователей", () -> {
+            fillRequiredFieldsAndClickCancel(name, password);
+            return getPage(UsersPage.class);
+        });
     }
 
     public CreateUserPage fillRequiredFieldsAndClickCancel(String name, String password) {
-        usernameField.shouldBe(visible, enabled).setValue(name);
-        newPasswordField.shouldBe(visible, enabled).setValue(password);
-        confirmPasswordField.shouldBe(visible, enabled).setValue(password);
-        cancelButton.click();
-        return this;
+        return StepLogger.log("Заполнить обязательные поля и нажать на кнопку Cancel", () -> {
+            usernameField.shouldBe(visible, enabled).setValue(name);
+            newPasswordField.shouldBe(visible, enabled).setValue(password);
+            confirmPasswordField.shouldBe(visible, enabled).setValue(password);
+            cancelButton.click();
+            return this;
+        });
     }
 }
