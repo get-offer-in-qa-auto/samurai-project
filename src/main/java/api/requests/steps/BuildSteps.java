3db5e5bf610abf8e1b5465b20987228948c8dfd6
paulina-pav <tilemzeyger.polina@gmail.com>
2025-12-27 16:25:40 +0700

    исправления чекстайла

--- src/main/java/api/requests/steps/BuildSteps.java
+++ src/main/java/api/requests/steps/BuildSteps.java
@@ -2,11 +2,7 @@ package api.requests.steps;
 
 import api.models.BaseModel;
 import api.models.buildConfiguration.BuildType;
-import api.models.builds.BuildQueueResponse;
-import api.models.builds.CancelBuildRequest;
-import api.models.builds.CreateBuildRequest;
-import api.models.builds.CreateBuildResponse;
-import api.models.builds.GetBuildResponse;
+import api.models.builds.*;
 import api.requests.skelethon.Endpoint;
 import api.requests.skelethon.requesters.CrudRequester;
 import api.requests.skelethon.requesters.ValidatedCrudRequester;
@@ -21,30 +17,30 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class BuildSteps {
     public static CreateBuildResponse addBuildToQueue(String buildTypeId) {
 
-        return StepLogger.log("Админ добавляет билд в очередь по buildTypeId: " + buildTypeId, ()-> {
-        CreateBuildRequest build = CreateBuildRequest.builder()
-                .buildType(
-                      BuildType.builder()
-                              .id(buildTypeId)
-                             .build()
-              )
-              .build();
-         CreateBuildResponse createdBuild = new ValidatedCrudRequester<CreateBuildResponse>(
-            RequestSpecs.adminAuthSpec(),
-            Endpoint.BUILD_QUEUE,
-            ResponseSpecs.requestReturnsOK()).post(build);
-         return createdBuild;
+        return StepLogger.log("Админ добавляет билд в очередь по buildTypeId: " + buildTypeId, () -> {
+            CreateBuildRequest build = CreateBuildRequest.builder()
+                    .buildType(
+                            BuildType.builder()
+                                    .id(buildTypeId)
+                                    .build()
+                    )
+                    .build();
+            CreateBuildResponse createdBuild = new ValidatedCrudRequester<CreateBuildResponse>(
+                    RequestSpecs.adminAuthSpec(),
+                    Endpoint.BUILD_QUEUE,
+                    ResponseSpecs.requestReturnsOK()).post(build);
+            return createdBuild;
         });
     }
 
     public static GetBuildResponse getBuildFromQueue(CreateBuildResponse response) {
-        return StepLogger.log("Админ возвращает билд " + response.getId() + " из очереди" , ()-> {
+        return StepLogger.log("Админ возвращает билд " + response.getId() + " из очереди", () -> {
             GetBuildResponse getBuildResponse = new ValidatedCrudRequester<GetBuildResponse>(
                     RequestSpecs.userAuthSpecWithToken(),
                     Endpoint.GET_BUILD,
                     ResponseSpecs.requestReturnsOK()).get(response.getId());
             return getBuildResponse;
-                });
+        });
     }
 
     public static void checkIfBuildHasAlreadyDeleted(CreateBuildResponse response) {
@@ -57,7 +53,7 @@ public class BuildSteps {
     }
 
     public static void deleteBuildFromQueue(CreateBuildResponse response) {
-        StepLogger.log("Админ удаляет билд : " + response.getId() + "из очереди", ()-> {
+        StepLogger.log("Админ удаляет билд : " + response.getId() + "из очереди", () -> {
             new CrudRequester(
                     RequestSpecs.userAuthSpecWithToken(),
                     Endpoint.DELETE_BUILD_FROM_QUEUE,
@@ -66,7 +62,7 @@ public class BuildSteps {
     }
 
     public static String prepareCommentForCancellingBuild(String testName) {
-        return StepLogger.log("Админ написал комментарий для удаления билда ", ()-> {
+        return StepLogger.log("Админ написал комментарий для удаления билда ", () -> {
             return String.format(
                     "Canceled by API test [%s] at %s",
                     testName,
@@ -76,7 +72,7 @@ public class BuildSteps {
     }
 
     public static BuildQueueResponse getBuildQueue() {
-        return StepLogger.log("Админ возвращает всю очередь билдов ", ()-> {
+        return StepLogger.log("Админ возвращает всю очередь билдов ", () -> {
             return new ValidatedCrudRequester<BuildQueueResponse>(
                     RequestSpecs.userAuthSpecWithToken(),
                     Endpoint.GET_BUILDS_QUEUE,
@@ -86,7 +82,7 @@ public class BuildSteps {
     }
 
     public static void cancelBuild(CreateBuildResponse response) {
-        StepLogger.log("Админ отменяет билд: " + response.getId(), ()-> {
+        StepLogger.log("Админ отменяет билд: " + response.getId(), () -> {
             String comment = "Cancelled by automated test";
             CancelBuildRequest cancelBody = CancelBuildRequest.builder()
                     .comment(comment)
@@ -99,15 +95,16 @@ public class BuildSteps {
                     ResponseSpecs.requestReturnsOK()).post(response, response.getId());
         });
     }
-    public static void checkIfBuildIsAlreadyCanceled(CreateBuildResponse createdBuild){
-         StepLogger.log("Админ проверяет, что билд: " + createdBuild.getId() + " отменен", ()-> {
-             //убедиться, что билд действительно отменен и его нет в очереди (статус finished)
-             GetBuildResponse getBuildResponse = new ValidatedCrudRequester<GetBuildResponse>(
-                     RequestSpecs.userAuthSpecWithToken(),
-                     Endpoint.GET_BUILD,
-                     ResponseSpecs.requestReturnsOK()).get(createdBuild.getId());
 
-             assertThat(getBuildResponse.getState()).isEqualTo(FINISHED.getMessage());
-         });
+    public static void checkIfBuildIsAlreadyCanceled(CreateBuildResponse createdBuild) {
+        StepLogger.log("Админ проверяет, что билд: " + createdBuild.getId() + " отменен", () -> {
+            //убедиться, что билд действительно отменен и его нет в очереди (статус finished)
+            GetBuildResponse getBuildResponse = new ValidatedCrudRequester<GetBuildResponse>(
+                    RequestSpecs.userAuthSpecWithToken(),
+                    Endpoint.GET_BUILD,
+                    ResponseSpecs.requestReturnsOK()).get(createdBuild.getId());
+
+            assertThat(getBuildResponse.getState()).isEqualTo(FINISHED.getMessage());
+        });
     }
 }
