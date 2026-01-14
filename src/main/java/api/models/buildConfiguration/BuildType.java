10e07f6592d731d8fa4995383d297f066d648d1e
SoloNadin <story81@bk.ru>
2026-01-14 09:24:58 +0400

    fix/ci-compilation-errors

---
+++ src/main/java/api/models/buildConfiguration/BuildType.java
@@ -0,0 +1,18 @@
+package api.models.buildConfiguration;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BuildType {
+    private String id;
+    private String name;
+    private String projectName;
+    private String projectId;
+    private String href;
+    private String webUrl;
+}
