5a516229b095aee041761b75c7c9af99985be180
SoloNadin <story81@bk.ru>
2026-01-15 13:12:15 +0400

    test

--- src/test/java/ui/AuthorizationSuperUserTest.java
+++ src/test/java/ui/AuthorizationSuperUserTest.java
@@ -1,12 +1,25 @@
 package ui;
 
+import api.configs.Config;
+import com.codeborne.selenide.Configuration;
 import common.helpers.TeamCityTokenUpdater;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ui.pages.authorization.FirstStartPage;
 import ui.pages.authorization.SetUpAdminPage;
 
 public class AuthorizationSuperUserTest extends BaseUiTest {
+
+    @BeforeAll
+    static void printConfig() {
+        System.out.println("=== TEST CONFIG ===");
+        System.out.println("SERVER = " + Config.getProperty("server"));
+        System.out.println("UIREMOTE = " + Config.getProperty("uiRemote"));
+
+        Configuration.baseUrl = Config.getProperty("server");
+        Configuration.remote = Config.getProperty("uiRemote");
+    }
     @Test
     public void setUpTeamCity() {
         SetUpAdminPage adminPage = new FirstStartPage()
